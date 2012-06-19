b9de5ab450e1af5091306462958a3125056202fc
Jaci <Jaci@youremail.com>
2012-06-18 19:14:47 -0500

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,21 @@
 require 'erb'
 
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
+x = 7000
+y = "Jaci "
+z = "is learning Ruby"
+template = ERB.new "The value of this class is: <%= y + z %>"
+lineone = ERB.new "<%= y + z%>"
+linetwo = ERB.new "<%= x %>"
+pets = ['Clementine', 'Ande']
+
+answer = ERB.new "Jaci does not have <%= pets.count %> pets"
+test = ERB.new '
+<% pets.each do |pet| %>
+   <%= "I love #{pet}" %> 
+<% end %>'
+puts answer.result(binding)
+puts test.result(binding)
 puts template.result(binding)
+puts lineone.result(binding)
+puts linetwo.result(binding)
+
